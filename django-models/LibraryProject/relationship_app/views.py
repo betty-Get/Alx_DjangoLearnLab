3c40a6e6f4fbb178da71aa422789802dc078e207
bettyGpt <betelhem.getachew@girlpowertalk.com>
2025-07-19 12:51:24 +0300

    template added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# Function-based view to list all books
+from django.http import HttpResponse
+from .models import Book
 
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
-
-# Class-based view to show library details and its books
-
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    output = '\n'.join(
+        [f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
